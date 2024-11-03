f430087ae64e4341f7e91552856f0c36c51e76f7
PELERIN Aurélien <pelerin.aurelien@pelerinsoftware.com>
2024-11-03 18:25:30 +0100

    modifiePremierInexistant test

--- src/main/java/liste/ListeSimple.java
+++ src/main/java/liste/ListeSimple.java
@@ -130,8 +130,7 @@ public class ListeSimple {
             precedentR2 = getPrecedent(r2);
             precedentR2.setSuivant(tete);
             tete = r2;
-        }
-        else if (r2 == tete) {
+        } else {
             precedentR1 = getPrecedent(r1);
             precedentR1.setSuivant(tete);
             tete = r1;
